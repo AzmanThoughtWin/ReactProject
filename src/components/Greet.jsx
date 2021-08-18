dc8b5ef56b829675d6a8787e97dd2075c8295c04
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-18 11:36:46 +0530

    setState

--- src/components/Greet.jsx
+++ src/components/Greet.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export const Greet = () =>  <h1>Hello Azman</h1>;
+export const Greet = props => {
+    console.log(props);
+    return (
+        <div>
+            <h1 className="p2">Hello {props.name} aka {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
+}  
 
 // export default Greet;
