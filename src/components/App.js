c60c5998bd4ff427331a5c6020fc3913ebdf3137
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-19 19:12:45 +0530

    Styling and CSS basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import NameList from './NameList';
+import Inline from './Inline';
+// import Stylesheet from './Stylesheet';
+// import NameList from './NameList';
 // import UserGreeting from './UserGreeting';
 // import ParentComponent from './ParentComponent';
 // import { Greet } from './Greet';
@@ -10,12 +12,18 @@ import NameList from './NameList';
 // import FunctionClick from './FunctionClick';
 // import ClassClick from './ClassClick';
 // import EventBind from './EventBind';
+import '../appStyles.css';
+import styles from '../appStyles.module.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <NameList/>
+        <h1 className="error">Error</h1>
+        <h1 className={styles.Success}>Success</h1>
+        <Inline/>
+        {/* <Stylesheet primary={true} /> */}
+        {/* <NameList/> */}
         {/* <UserGreeting/> */}
         {/* <ParentComponent/> */}
         {/* <EventBind/> */}
