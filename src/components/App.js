1d86f4ba1f164dc4d9b4cdbfb8cd4adf8db685e6
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-20 17:20:35 +0530

    Learn 2 approches of creating  Refs

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import ParentComp from './ParentComp';
-
-
+import RefsDemo from './RefsDemo';
 
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <ParentComp/>
+        <RefsDemo/>
       </div>
     );
   }
