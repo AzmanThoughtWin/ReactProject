6f476ec5ceca3e1b015d850db0f0ec5fe516eeae
AzmanThoughtWin <azman.thoughtwin@gmail.com>
2021-08-20 19:26:11 +0530

    Forwarding Refs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import FocusInput from './FocusInput';
+import FRParentInput from './FRParentInput';
 
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <FocusInput/>
+        <FRParentInput/>
       </div>
     );
   }
